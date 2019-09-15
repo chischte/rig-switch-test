8b604333ef701ddc6383d12018b5f0e65d58d428
realslimshady <chischte@gmx.ch>
2019-09-15 02:06:23 +0200

    Clean up, function to be tested

--- SWITCH_TEST/Nextion.ino
+++ SWITCH_TEST/Nextion.ino
@@ -1,154 +1,136 @@
 /*
  * *****************************************************************************
- nextion.ino
- configuration of the Nextion touch display
- Michael Wettstein
- September 2019, Zürich
+ * Nextion.ino
+ * configuration of the Nextion touch display
+ * Michael Wettstein
+ * September 2019, Zürich
  * *****************************************************************************
- an xls-sheet to generate Nextion events can be found here:
- https://github.com/chischte/push-type-rig/PUSHTYPE_NEXTION/
+ * An Excel-sheet to generate Nextion events can be found here:
+ * https://github.com/chischte/push-type-rig/PUSHTYPE_NEXTION/
+ * *****************************************************************************
+ * Include the Nextion ITEADLIB library https://github.com/itead/ITEADLIB_Arduino_Nextion
+ * Make sure you edit the NexConfig.h file on the library folder to set the correct
+ * serial port for the display.
+ * By default it's set to Serial1, which most Arduino boards don't have.
+ * Change "#define nexSerial Serial1" to "#define nexSerial Serial" if you are
+ * using Arduino uno, nano, etc.
+ * *****************************************************************************
+ * VARIOUS COMMANDS:
+ * *****************************************************************************
+ * Serial2.print("click bt1,1");//CLICK BUTTON
+ * send_to_nextion();
+ * A switch (Dual State Button) will be toggled with this command,
+ * a Button will be set permanently pressed
+ * Serial2.print("vis t0,0");// HIDE OBJECT
+ * send_to_nextion();
+ * Serial2.print("t0.txt=");// WRITE TEXT:
+ * Serial2.print("\"");
  * *****************************************************************************
  */
 
-// Include the nextion library (the official one) https://github.com/itead/ITEADLIB_Arduino_Nextion
-// Make sure you edit the NexConfig.h file on the library folder to set the correct serial port for the display.
-// By default it's set to Serial1, which most arduino boards don't have.
-// Change "#define nexSerial Serial1" to "#define nexSerial Serial" if you are using arduino uno, nano, etc.
-//**************************************************************************************
-//VARIOUS COMMANDS:
-//**************************************************************************************
-// Serial2.print("click bt1,1");//CLICK BUTTON
-// send_to_nextion();
-// A switch (Dual State Button)will be toggled with this command, a Button will be set permanently pressed)
-// Serial2.print("vis t0,0");//HIDE OBJECT
-// send_to_nextion();
-// Serial2.print("t0.txt=");//WRITE TEXT:
-// Serial2.print("\"");
-// Serial2.print("SCHNEIDEN");
-// Serial2.print("\"");
-// send_to_nextion();
-//**************************************************************************************
-//DECLARATION OF VARIABLES
-//**************************************************************************************
-//***************************************************************************
-//NEXTION SWITCH STATES LIST
-//Every nextion switch button (dualstate) needs a switchstate variable to control switchtoggle
-//Nextion buttons(momentary) need a variable too, to prevent screen flickering
-bool nex_state_machineRunning;
-bool reset_stopwatch_active = false;
-
-//***************************************************************************
-bool nex_prev_stepMode = true;
+//*****************************************************************************
+// DECLARATION OF VARIABLES
+//*****************************************************************************
+bool resetStopwatchActive = false;
 int currentPage = 0;
-unsigned long counter_reset_stopwatch;
-//**************************************************************************************
-//DECLARATION OF OBJECTS TO BE READ FROM NEXTION
-//**************************************************************************************
+unsigned long counterResetStopwatch;
 
-//PAGE 0:
+//*****************************************************************************
+// DECLARATION OF OBJECTS TO BE READ FROM NEXTION
+//*****************************************************************************
+// PAGE 0:
 NexDSButton nex_switch_play_pause = NexDSButton(0, 2, "bt0");
 
-//**************************************************************************************
+//*****************************************************************************
 //END OF OBJECT DECLARATION
-//**************************************************************************************
+//*****************************************************************************
 
-char buffer[100] = { 0 }; // This is needed only if you are going to receive a text from the display. You can remove it otherwise.
+char buffer[100] =
+{ 0 }; // This is needed only if you are going to receive a text from the display.
 
-//**************************************************************************************
+//*****************************************************************************
 //TOUCH EVENT LIST //DECLARATION OF TOUCH EVENTS TO BE MONITORED
-//**************************************************************************************
-NexTouch *nex_listen_list[] = { &nex_switch_play_pause,
+//*****************************************************************************
+NexTouch *nex_listen_list[] =
+{ &nex_switch_play_pause,
 
 NULL //String terminated
-        };
-//**************************************************************************************
+    };
+//*****************************************************************************
 //END OF TOUCH EVENT LIST
-//**************************************************************************************
+//*****************************************************************************
 
-void send_to_nextion() {
-    Serial2.write(0xff);
-    Serial2.write(0xff);
-    Serial2.write(0xff);
+void send_to_nextion()
+{
+  Serial2.write(0xff);
+  Serial2.write(0xff);
+  Serial2.write(0xff);
 }
 
-//**************************************************************************************
+//*****************************************************************************
 void nextionSetup()
-//**************************************************************************************
+//*****************************************************************************
 {
-    Serial.println("START OF NEXTION SETUP");
-    Serial2.begin(9600);  // Start serial comunication at baud=9600
+  Serial.println("START OF NEXTION SETUP");
+  Serial2.begin(9600);  // Start serial comunication at baud=9600
 
-    //**************************************************************************************
-    //REGISTER THE EVENT CALLBACK FUNCTIONS
-    //**************************************************************************************
-    //*****PUSH::
-    // n.a.
+  //*****************************************************************************
+  //REGISTER THE EVENT CALLBACK FUNCTIONS
+  //*****************************************************************************
+  //*****PUSH::
+  // n.a.
 
-    //*****PUSH+POP:
-    nex_switch_play_pause.attachPush(nex_switch_play_pausePushCallback);
-    nex_switch_play_pause.attachPop(nex_switch_play_pausePopCallback);
+  //*****PUSH+POP:
+  nex_switch_play_pause.attachPush(nex_switch_play_pausePushCallback);
+  nex_switch_play_pause.attachPop(nex_switch_play_pausePopCallback);
 
-    //**************************************************************************************
-    //END OF REGISTER
-    //**************************************************************************************
-    delay(2000);
-    send_to_nextion();
-    Serial.println("END OF NEXTION SETUP");
+  //*****************************************************************************
+  //END OF REGISTER
+  //*****************************************************************************
+  delay(2000);
+  send_to_nextion();
+  Serial.println("END OF NEXTION SETUP");
 
 }  //END OF NEXTION SETUP
 
-//**************************************************************************************
+//*****************************************************************************
 void nextionLoop()
-//**************************************************************************************
-{ //START NEXTION LOOP
-//**************************************************************************************
-    nexLoop(nex_listen_list); //check for any touch event
-    //**************************************************************************************
-    if (currentPage == 0) {
-        //*******************
-        //PAGE 1:
-        //*******************
-
-        //UPDATE SWITCHSTATE "PLAY"/"PAUSE"
-        if (nex_state_machineRunning != machineRunning) {
-            Serial2.print("click bt0,1");    //CLICK BUTTON
-            send_to_nextion();
-            nex_state_machineRunning = !nex_state_machineRunning;
-        }
-        //RESET COUNTER:
-        if (reset_stopwatch_active == true) {
-            if (millis() - counter_reset_stopwatch > 3000) {
-                switchCounter.set(longTimeCounter, 0);
-                updateDisplayCounter();
-            }
-        }
+//*****************************************************************************
+{
+  nexLoop(nex_listen_list); //check for any touch event
+  if (currentPage == 0)
+  {
+    //********************************
+    // PAGE 0:
+    //********************************
+    // RESET COUNTER:
+    if (resetStopwatchActive == true)
+    {
+      if (millis() - counterResetStopwatch > 3000)
+      {
+        switchCounter.set(longTimeCounter, 0);
+        updateDisplayCounter();
+      }
     }
+  }
 }    //END OF NEXTION LOOP
 
-//**************************************************************************************
-//TOUCH EVENT FUNCTIONS //PushCallback = Press event //PopCallback = Release event
-//**************************************************************************************
+//*****************************************************************************
+//TOUCH EVENT FUNCTIONS
+//*****************************************************************************
 
-void nex_switch_play_pausePushCallback(void *ptr) {
-
-    machineRunning = !machineRunning;
-    if (machineRunning == true) {
-        clearanceNextStep = true;
-    }
-    if (machineRunning == false) {
-
-    }
-    nex_state_machineRunning = !nex_state_machineRunning;
-
-    counter_reset_stopwatch = millis();
-    reset_stopwatch_active = true;
-
+void nex_switch_play_pausePushCallback(void *ptr)
+{
+  machineRunning = !machineRunning;
+  counterResetStopwatch = millis();
+  resetStopwatchActive = true;
 }
 
-void nex_switch_play_pausePopCallback(void *ptr) {
-    reset_stopwatch_active = false;
+void nex_switch_play_pausePopCallback(void *ptr)
+{
+  resetStopwatchActive = false;
 }
 
-//**************************************************************************************
+//*****************************************************************************
 //END OF TOUCH EVENT FUNCTIONS
-//**************************************************************************************
+//*****************************************************************************
--- SWITCH_TEST/SWITCH_TEST.ino
+++ SWITCH_TEST/SWITCH_TEST.ino
@@ -1,26 +1,27 @@
 /*
  * *****************************************************************************
- SWITCH_TEST
+ * SWITCH_TEST
  * *****************************************************************************
- Program for a mechanical switch endurance test
+ * Program for an endurance test of a mechanical switch
  * *****************************************************************************
- Michael Wettstein
- September 2019, Zürich
+ * Michael Wettstein
+ * September 2019, Zürich
  * *****************************************************************************
  */
+
 /*
- TODO:
- Write EEPROM library
- Implement debounce algorithm
+ * TODO:
+ * Dont start/stop the machine if button is pushed for a counter reset
  */
 
 #include <Nextion.h>  // https://github.com/itead/ITEADLIB_Arduino_Nextion
-#include <Debounce.h> // ...add github link
-#include <EEPROM_Counter.h>
+#include <Debounce.h> // https://github.com/chischte/debounce-library.git
+#include <EEPROM_Counter.h> // https://github.com/chischte/eeprom-counter-library.git
 
-enum counter {
-    longTimeCounter,    // example value name
-    endOfEnum
+enum counter
+{
+  longTimeCounter, // name of the counter
+  endOfEnum //end of the counter list
 };
 int numberOfValues = endOfEnum;
 
@@ -29,46 +30,45 @@ int numberOfValues = endOfEnum;
 // EEPROM size Arduino Mega:     4096 bytes (ATmega2560)
 int eepromSize = 4096;
 
-// CREATE AN INSTANCE OF THE LIBRARY CLASS:
+// CREATE THE EEPROM COUNTER:
 EEPROM_Counter switchCounter(eepromSize, numberOfValues);
 
 //*****************************************************************************
-//DECLARATION OF VARIABLES / DATA TYPES
+// DECLARATION OF VARIABLES / DATA TYPES
 //*****************************************************************************
-//bool (true/false)
-//byte (0-255)
-//int   (-32,768 to 32,767) / unsigned int: 0 to 65,535
-//long  (-2,147,483,648 to 2,147,483,647)
-//float (6-7 Digits)
+// bool (true/false)
+// byte (0-255)
+// int   (-32,768 to 32,767)
+// long  (-2,147,483,648 to 2,147,483,647)
+// float (6-7 Digits)
 //*****************************************************************************
 
-//KNOBS AND POTENTIOMETERS:
+// KNOBS AND POTENTIOMETERS:
 const byte TEST_SWITCH_PIN = 2;
 const byte MOTOR_RELAY_PIN = 50;
-//SENSORS:
+// SENSORS:
 
-//OTHER VARIABLES:
+// n.a.
+
+// OTHER VARIABLES:
 bool debouncedButtonState;
-bool currentButtonState;
 bool previousButtonState;
 bool machineRunning = false;
-bool clearancePlayPauseToggle = true;
-bool clearanceNextStep = false;
 
 unsigned long runtime;
 unsigned long runtimeStopwatch;
-unsigned long prev_time;
 
 Debounce testSwitch(TEST_SWITCH_PIN);
 
 //*****************************************************************************
-void updateDisplayCounter() {
-    long newValue = switchCounter.getValue(longTimeCounter);
-    Serial2.print("t0.txt=");
-    Serial2.print("\"");
-    Serial2.print(newValue);
-    Serial2.print("\"");
-    send_to_nextion();
+void updateDisplayCounter()
+{
+  long newValue = switchCounter.getValue(longTimeCounter);
+  Serial2.print("t0.txt=");
+  Serial2.print("\"");
+  Serial2.print(newValue);
+  Serial2.print("\"");
+  send_to_nextion();
 }
 //*****************************************************************************
 //******************######**#######*#######*#******#*######********************
@@ -77,13 +77,14 @@ void updateDisplayCounter() {
 //***********************#**#**********#****#******#*#*************************
 //*****************######***######*****#*****######**#*************************
 //*****************************************************************************
-void setup() {
-    Serial.begin(9600); //start serial connection
-    pinMode(TEST_SWITCH_PIN, INPUT_PULLUP);
-    pinMode(MOTOR_RELAY_PIN, OUTPUT);
-    nextionSetup();
-    updateDisplayCounter();
-    Serial.println("EXIT SETUP");
+void setup()
+{
+  Serial.begin(9600); // start serial connection
+  pinMode(TEST_SWITCH_PIN, INPUT_PULLUP);
+  pinMode(MOTOR_RELAY_PIN, OUTPUT);
+  nextionSetup();
+  updateDisplayCounter();
+  Serial.println("EXIT SETUP");
 }
 //*****************************************************************************
 //********************#*********#####***#####***######*************************
@@ -93,24 +94,25 @@ void setup() {
 //********************#######***#####***#####***#******************************
 //*****************************************************************************
 
-void loop() {
+void loop()
+{
+  // GET INFOS FROM TOUCH DISPLAY:
+  nextionLoop();
 
-    //GET INFOS FROM TOUCH DISPLAY:
-    nextionLoop();
+  // SWITCH MOTOR ON AND OFF:
+  digitalWrite(MOTOR_RELAY_PIN, machineRunning);
 
-    //SWITCH MOTOR ON AND OFF:
-    digitalWrite(MOTOR_RELAY_PIN, machineRunning);
-
-    //GET SIGNAL FROM TEST SWITCH AND COUNT IT
-    bool debouncedButtonState = testSwitch.requestButtonState();
-    if (previousButtonState != debouncedButtonState) {
-        if (debouncedButtonState == LOW) {
-            switchCounter.countOneUp(longTimeCounter);
-            updateDisplayCounter();
-        }
-        previousButtonState = debouncedButtonState;
+  // GET SIGNAL FROM TEST SWITCH AND COUNT IT:
+  bool debouncedButtonState = testSwitch.requestButtonState();
+  if (previousButtonState != debouncedButtonState)
+  {
+    if (debouncedButtonState == LOW)
+    {
+      switchCounter.countOneUp(longTimeCounter);
+      updateDisplayCounter();
     }
-
+    previousButtonState = debouncedButtonState;
+  }
 
 //runtime = millis() - runtimeStopwatch;
 //Serial.println(runtime);
